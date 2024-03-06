5080b7ae6ab6bf3701315857367a933008a32acb
DiniduNethsara0423 <dinidubandara2004@gmail.com>
2024-03-06 19:04:48 +0530

    initil

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const AppContent = () => {
       <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
       <h1 className="title">Hello world!</h1>
       <div className="box">
-        <h2>This is a box</h2>
+        <h2>Hello</h2>
       </div>
     </div>
   );
